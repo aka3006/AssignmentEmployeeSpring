b9689678c50e369f5692f02ecbf65dd4f1f3ee43
aka3006 <60283319+aka3006@users.noreply.github.com>
2020-02-07 10:45:39 +0530

    Update AssignmentEmployeeSpringApplication.java

--- src/main/java/com/cognizant/AssignmentEmployeeSpring/AssignmentEmployeeSpringApplication.java
+++ src/main/java/com/cognizant/AssignmentEmployeeSpring/AssignmentEmployeeSpringApplication.java
@@ -2,6 +2,7 @@ package com.cognizant.AssignmentEmployeeSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = com.aka.Employee.EmployeeController.class)
